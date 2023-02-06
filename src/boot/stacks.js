eb1a61eaa0e1b90acf0733cdb7c456c21e9c9301
Dan Trevino <dantrevino@gmail.com>
2023-02-05 18:26:15 -0600

    initial commit

---
+++ src/boot/stacks.js
@@ -0,0 +1,10 @@
+import { boot } from "quasar/wrappers";
+import { AppConfig, UserSession } from "@stacks/connect";
+
+const appConfig = new AppConfig(["store_write", "publish_data"]);
+// "async" is optional;
+// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
+export default boot(async (/* { app, router, ... } */) => {
+  // something to do
+});
+export const userSession = new UserSession({ appConfig });
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page class="flex flex-center">
+    <StacksConnectWallet />
+  </q-page>
+</template>
+
+<script setup>
+import StacksConnectWallet from "../components/StacksConnectWallet.vue";
+</script>
