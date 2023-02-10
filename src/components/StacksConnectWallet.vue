4a54eaefe991f098780ece612c56cb4aa5f58d59
pratikpatel <pratikpatelpp802@gmail.com>
2023-02-10 13:59:40 +0530

    Some design changes

--- src/components/StacksConnectWallet.vue
+++ src/components/StacksConnectWallet.vue
@@ -1,7 +1,7 @@
 <script setup>
-import { showConnect } from "@stacks/connect";
-import { userSession } from "../boot/stacks";
-import { computed } from "vue";
+import {showConnect} from "@stacks/connect";
+import {userSession} from "../boot/stacks";
+import {computed} from "vue";
 
 function authenticate() {
   showConnect({
@@ -39,14 +39,31 @@ function handleLoginLogout() {
 </script>
 
 <template>
-  <div
-    class="row bg-grey-3 text-dark cursor-pointer"
-    style="height: 36px; border-radius: 50px"
-    @click="handleLoginLogout"
-  >
-    <span class="text-body1 q-pa-sm text-bold">{{ buttonMsg }}</span>
-    <img :src="buttonIcon" height="36px" />
-  </div>
+  <q-btn round dense flat color="white" icon="notifications">
+    <q-avatar size="32px">
+      <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+    </q-avatar>
+    <q-menu class="no-shadow menu-border"
+    >
+      <q-card class="no-shadow" style="min-width: 250px">
+        <q-card-section class="text-center text-grey-8">
+          Logged in as #StackAddress
+        </q-card-section>
+        <q-separator></q-separator>
+        <q-card-section class="q-pa-sm">
+          <q-btn :label="buttonMsg"  @click="handleLoginLogout" rounded outline no-caps icon="logout" class="full-width"></q-btn>
+        </q-card-section>
+      </q-card>
+    </q-menu>
+  </q-btn>
+<!--  <div-->
+<!--    class="row bg-grey-3 text-dark cursor-pointer"-->
+<!--    style="height: 36px; border-radius: 50px"-->
+<!--    @click="handleLoginLogout"-->
+<!--  >-->
+<!--    <span class="text-body1 q-pa-sm text-bold">{{ buttonMsg }}</span>-->
+<!--    <img :src="buttonIcon" height="36px" />-->
+<!--  </div>-->
 </template>
 
 <style scoped></style>
