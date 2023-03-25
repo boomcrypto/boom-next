178946f0d3396a34463ee39d219d56c3cc828a89
Dan Trevino <dantrevino@gmail.com>
2023-03-25 18:55:50 -0500

    add error view

---
+++ src/components/Collections/CollectionsErrorView.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-card
+    flat
+    class="bg-transparent q-mx-auto"
+    style="max-width: 1080px; border-radius: 24px; height: 83vh; margin: 0 auto"
+  >
+    <q-card flat class="boom-card absolute-center text-center">
+      <q-img src="/appicons/sad.svg" width="200px" alt="Mint icon" />
+      <q-card-section>
+        No collections yet! Mint your first masterpiece one-of-one collection
+        now.
+      </q-card-section>
+    </q-card>
+  </q-card>
+</template>
+
+<script setup></script>
